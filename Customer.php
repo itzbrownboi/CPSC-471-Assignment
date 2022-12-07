5a2ad6a1c66498874e74ef3be37ff8aea6fe9808
itzbrownboi <itzbrownboi@gmail.com>
2022-12-07 08:52:12 -0700

    Update Customer.php

--- Customer.php
+++ Customer.php
@@ -8,10 +8,18 @@ if (mysqli_connect_errno($con))
 }
 
 $txtCustomer_ID = $_POST['txtCustomer_ID'];
-$txtRefID = NULL;
-$RefIDSQL = "SELECT reference_num FROM disputes";
+$txtRefID = rand();
+$RefIDSQL = "SELECT reference_num FROM disputes WHERE '$txtRefID' NOT IN reference_num";
 $tempresult = mysqli_query($con, $RefIDSQL);
 $tempRow = mysqli_fetch_array($tempresult);
+
+while(is_array($tempRow)){
+    $txtRefID = rand();
+    $RefIDSQL = "SELECT reference_num FROM disputes WHERE '$txtRefID' NOT IN reference_num";
+    $tempresult = mysqli_query($con, $RefIDSQL);
+    $tempRow = mysqli_fetch_array($tempresult);
+}
+
 // The RefID should do a random number and if it is already take, it should come up with anew random number. Help me if you can
 //If y'all can figure it out let me know
 
@@ -20,6 +28,19 @@ $currentRefrencesSQL = "SELECT reference_num, ref_type, customer_id FROM dispute
 $result = mysqli_query($con, $currentRefrencesSQL);
 $display = mysqli_fetch_array($result);
 
+//Show Customer details
+$cutomer_informationSQL = "SELECT end_user_id, FName, LName, user_email  FROM end_user WHERE customer_id == '$txtCustomer_ID ";
+$customer_cardSQL = "SELECT card_number, payment_info, customer_id FROM customer WHERE customer_id == '$txtCustomer_ID ";
+$customer_reservationSQL = "SELECT =reservation_num, seat_type, check_in, carry_on, reservation_date, flight_number FROM reservation WHERE customer_id == '$txtCustomer_ID ";
+
+//run above sql
+$res1 = mysqli_query($con, $customer_informationSQL);
+$res2 = mysqli_query($con, $customer_cardSQL);
+$res3 = mysqli_query($con, $customer_reservationSQL);
+
+$entry1 = mysqli_fetch_array($res1);
+$entry2 = mysqli_fetch_array($res2);
+$entry3 = mysqli_fetch_array($res3);
 //Insert additonal Dispute if required
 
 //Need if statement from HTML to determine whether this is ran or not.
